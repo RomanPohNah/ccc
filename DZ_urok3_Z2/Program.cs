928fff680c924984a5e8e828cf9684e0d0f5d52e
Roman <romanpokolenko@yandex.ru>
2023-12-20 22:08:18 +0900

    dz

---
+++ DZ_urok3_Z2/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] array = {15, 8, 3, 17, 6,34, 42, 46, 22, 91};
+
+int evenCount = 0;
+
+for(int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0)
+    {
+        evenCount++;
+    }
+}
+Console.WriteLine(evenCount);
