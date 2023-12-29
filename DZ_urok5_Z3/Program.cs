cb8a929657b35b9c2437bf67d876b24ea23606a5
Roman <romanpokolenko@yandex.ru>
2023-12-29 22:48:21 +0900

    dz_5_2

---
+++ DZ_urok5_Z3/Program.cs
@@ -0,0 +1,31 @@
+﻿int[,] array = {
+    {3,5,6},
+    {9,6,7},
+    {3,7,2}
+};
+Console.WriteLine("ввод");
+PrintArray(array);
+SwapRows(array, 0, array.GetLength(0) - 1);
+Console.WriteLine("после обмена");
+PrintArray(array);
+void SwapRows(int[,] array, int row1, int row2)
+{
+    int cols = array.GetLength(1);
+    for (int i = 0; i < cols; i++)
+    {
+        int temp = array[row1, i];
+        array[row1, i] = array[row2, i];
+        array[row2, i] = temp;
+    }
+}
+void PrintArray(int[,] array)
+{
+    int rows = array.GetLength(0);
+    int cols = array.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int J = 0; J < cols; J++)
+        Console.WriteLine(array[i,J] + " ");
+    }
+    Console.WriteLine();
+}
