8ac2c3343c86150fc75f5d285945fadb92668cb6
Roman <romanpokolenko@yandex.ru>
2024-01-03 14:15:13 +0900

    dz_rekursiaRevers

---
+++ DZ_rekursia2/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] Array = { 1, 2, 3, 4, 5, 6, 7 };
+Console.WriteLine(" массив в обратном порядке:");
+PrintArrayReverse(Array, Array.Length - 1);
+Console.ReadLine();
+
+static void PrintArrayReverse(int[] array, int index)
+{
+    if (index >= 0)
+    {
+        Console.WriteLine(array[index]);
+        PrintArrayReverse(array, index - 1);
+    }
+}
