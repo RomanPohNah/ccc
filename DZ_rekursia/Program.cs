91f326159661f8b8815253ac6810667d38c62bdf
Roman <romanpokolenko@yandex.ru>
2024-01-03 13:50:09 +0900

    dz_rekursia1

---
+++ DZ_rekursia/Program.cs
@@ -0,0 +1,15 @@
+﻿    Console.WriteLine("Введите M:");
+    int m = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите N:");
+    int n = Convert.ToInt32(Console.ReadLine());
+    PrintNaturalNumbers(m, n);
+    Console.ReadLine();
+static void PrintNaturalNumbers(int m, int n)
+{
+    if (m <= n)
+    {
+        Console.WriteLine(m);
+        PrintNaturalNumbers(m + 1, n);
+    }
+
+}
